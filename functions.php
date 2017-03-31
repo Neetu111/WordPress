1b1045cc330249fc8f77955f1771175ce6781f5c
Neetu111 <201451015@gmail.com>
2017-03-31 21:49:09 +0530

    second commit

--- functions.php
+++ functions.php
@@ -4,11 +4,6 @@ function learningWordPress_resources(){
 }
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 
-//Naviagation Menus
-register_nav_menus(array(
-	'primary' => __('Primary Menu'),
-	'footer' => __('Footer Menu'),
-));
 
 //Get Top Ancestor
 function get_top_ancestor_id(){
@@ -26,4 +21,29 @@ function has_children(){
 	$pages = get_pages('child_of='.$post->ID);
 	return count($pages);
 }
+
+// Customize excerpt word count length
+function custom_excerpt_length(){
+	return 10;
+}
+add_filter('excerpt_length', 'custom_excerpt_length');
+
+//Theme Setup
+function learningWordPres_setup(){
+	//Naviagation Menus
+	register_nav_menus(array(
+		'primary' => __('Primary Menu'),
+		'footer' => __('Footer Menu'),
+	));
+	
+	//Add featured image support
+	add_theme_support('post-thumbnails');
+	add_image_size('small-thumbnail', 180, 120, true);
+	add_image_size('banner-image', 920, 210, true);
+	
+	//Add post formate support
+	add_theme_support('post-formats', array('aside', 'gallery', 'link'));
+}
+add_action('after_setup_theme', 'learningWordPres_setup');
+
 ?>
